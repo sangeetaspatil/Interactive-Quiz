f4045eb211c4c9993013c0298d403d4a749ce9dd
sangeetaspatil <sangeetapatil.3602@gmail.com>
2023-12-05 21:39:39 +0530

    Quiz using html, css and javascript

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+{
+  numb: 1,
+  question: "Which type of JavaScript language is ___",
+  answer: "Object-Based",
+  options: [
+    "Object-Oriented",
+    "Object-Based",
+    "Assembly-language",
+    "High-level"
+  ]
+},
+  {
+  numb: 2,
+  question: "Which one of the following also known as Conditional Expression:",
+  answer: "immediate if",
+  options: [
+    "Alternative to if-else",
+    "immediate if",
+    "If-then-else statement",
+    "Switch statement"
+  ]
+},
+  {
+  numb: 3,
+  question: " In JavaScript, what is a block of statement?",
+  answer: "block that combines a number of statements into a single compound statement",
+  options: [
+    "Conditional block",
+    "both conditional block and a single statement",
+    "block that contains a single statement",
+    "block that combines a number of statements into a single compound statement"
+  ]
+},
+{
+  numb: 4,
+  question: " In JavaScript, what is a block of statement?",
+  answer: "Both",
+  options: [
+    "var",
+    "let",
+    "Both",
+    "None of above"
+  ]
+},
+  {
+  numb: 5,
+  question: "What does XML stand for?",
+  answer: "eXtensible Markup Language",
+  options: [
+    "eXtensible Markup Language",
+    "eXecutable Multiple Language",
+    "eXTra Multi-Program Language",
+    "eXamine Multiple Language"
+  ]
+},
+];
